72546f12ec4e8984eb94b2c60c200736ff19306f
repl.it user <replituser@example.com>
2020-10-06 14:59:03 +0000

    reverse string

---
+++ basics/for_loop/reverse.py
@@ -0,0 +1,5 @@
+print("Ok, what phrase do you see?")
+phrase = input()
+print("Ok, let me reverse that for you.....")
+for position in range (len(phrase) -1, -1, -1):
+  print(phrase[position], end="")
