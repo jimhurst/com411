6629a2e5e73fb85d52b1e6326c81a474bb832448
repl.it user <replituser@example.com>
2020-10-06 11:21:59 +0000

    started review

---
+++ basics/decisions/review.py
@@ -0,0 +1,7 @@
+#PLANET QUIZ
+print("How many planets are in our solar system?")
+numplanets = int(input())
+if numplanets == '8':
+  print("nicely done, yes pluto is now classified as a dwarf planet")
+elif numplanets == '9':
+  print("Well yes, historically we did teach that, however pluto is now classified as a dwarf planet")
