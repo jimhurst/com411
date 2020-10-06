214ab92f1c1bb2b0541654aa46456fe076d7d8e9
repl.it user <replituser@example.com>
2020-10-06 13:49:07 +0000

    finished simple.py

---
+++ basics/while_loop/simple.py
@@ -0,0 +1,11 @@
+#User to input number of cables for removal
+print("How many cables should I remove?")
+#stored in variable
+cables = int(input())
+print(f"Ok starting with {cables} Cables")
+#removed cables variable set
+remcables = 0
+#while command to increment removed cables whilst this remains lower than requested number of cables for removal
+while (remcables < cables):
+  print("Removed cable")
+  remcables = remcables + 1
