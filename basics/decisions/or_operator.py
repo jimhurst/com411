3b2ca554413d96872401ba6bdde05c85ad6d3bed
repl.it user <replituser@example.com>
2020-10-06 11:06:24 +0000

    or_operator

---
+++ basics/decisions/or_operator.py
@@ -0,0 +1,12 @@
+#input to determine adventure type
+print("what type of adventure do you want")
+storytype = input()
+#if scary or short
+if ((storytype == "scary") or (storytype == "short")):
+  print("you are entering the dark forest!")
+#if safe or long
+elif ((storytype == "safe") or (storytype == "long")):
+  print("you are taking the safe route")
+#else
+else:
+  print("I don't know which route to take")
