9a5419532257af270c339d9cec3017abeba45d4f
repl.it user <replituser@example.com>
2020-10-06 15:22:45 +0000

    nested.py finished

---
+++ basics/nested_loop/nested.py
@@ -0,0 +1,11 @@
+#user input for number of rows
+print("How many rows should I have?")
+rows = int(input())
+#user input for number of columns
+print("How many columns should I have?")
+columns = int(input())
+#render output
+for row in range (0,rows,1):
+  for column in range (0,columns,1):
+    print(":-", end="")
+  print()
