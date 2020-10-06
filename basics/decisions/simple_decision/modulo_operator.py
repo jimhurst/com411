04619c334e33f02eb832399dd46a7d6d77be63f7
repl.it user <replituser@example.com>
2020-10-06 09:48:27 +0000

    module_operator

---
+++ basics/decisions/simple_decision/modulo_operator.py
@@ -0,0 +1,8 @@
+#ask user for whole number input
+print("Please enter a whole number of your choice")
+whole = int(input())
+calc = whole % 2
+if calc != 0:
+  print(f"the number {whole} is an odd number")
+else:
+  print(f"the number {whole} is an even number")
