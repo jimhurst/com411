05d17b61fba8bff2e6777577b3f475abbaa7dd84
repl.it user <replituser@example.com>
2020-10-06 11:13:53 +0000

    and_operator

---
+++ basics/decisions/and_operator.py
@@ -0,0 +1,11 @@
+#input - what was heard
+print("What did I hear?")
+hear = input()
+#input - what was seen
+print("What did I see?")
+see = input()
+#logic and output
+if ((hear.lower() == "grrr") and (see.lower() == "two red eyes")):
+  print("There is a scary creature. I should get out of here!")
+else:
+  print("I am a little scared but I will continue")
