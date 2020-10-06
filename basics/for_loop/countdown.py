fd3fb08b3a50ccda8281e1e701336a09d714822f
repl.it user <replituser@example.com>
2020-10-06 14:06:23 +0000

    countdown created

---
+++ basics/for_loop/countdown.py
@@ -0,0 +1,9 @@
+#User input on how far from the cave
+print("How far are we from the cave?")
+cavecount = int(input())
+#counting down the remaining steps
+for count in range(cavecount):
+  print(f"There are {cavecount} steps remaining")
+  cavecount = cavecount - 1
+print("")
+print("We have reached the cave!!!")
