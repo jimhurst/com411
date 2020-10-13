934c7b28d744862e8c8e7c9edbbb31bef1828d3b
repl.it user <replituser@example.com>
2020-10-13 10:44:57 +0000

    function_with_parameter

---
+++ basics/functions/function_with_parameter.py
@@ -0,0 +1,12 @@
+def escape_by(route):
+  if route == 'jumping over':
+    print("We cannot escape that way! The boulder is too big!")
+  elif route == 'running around':
+    print("We cannot escape that way!")
+  elif route == 'going deeper':
+    print("That might just work! Let's go deeper!")
+  else:
+    print("We cannot escape that way! The boulder is in the way!")
+escape_by("jumping over")
+escape_by("running around")
+escape_by("going deeper")
