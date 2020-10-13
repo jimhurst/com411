1e2d285c2cea78815aff276c19b9dc2748e99550
repl.it user <replituser@example.com>
2020-10-13 11:18:54 +0000

    finished fwp

--- basics/functions/function_with_parameters.py
+++ basics/functions/function_with_parameters.py
@@ -1,7 +1,8 @@
-def climb_ladder(steps_rem,steps_cro):
+def climb_ladder(steps_rem, steps_cro):
   if steps_rem > steps_cro:
     print("Still some way to go!")
   else:
-    print(We are almost there!)
+    print("We are almost there!")
 
 climb_ladder(5,2)
+climb_ladder(2,5)
