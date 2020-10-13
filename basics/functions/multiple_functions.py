7bb47d6bec807a59e3cdb543b30a0adc88cf25f0
repl.it user <replituser@example.com>
2020-10-13 11:31:12 +0000

    multiple_functions

---
+++ basics/functions/multiple_functions.py
@@ -0,0 +1,15 @@
+#define function to render ladder
+def display_ladder(steps):
+  for count in range(steps):
+    print(" |       | ")
+    print(" |       | ")
+    print(" +++++++++ ")
+
+#define function to ask for number of steps and call disply_ladder function
+def create_ladder():
+  print("please enter the number of steps")
+  steps = int(input())
+  display_ladder(steps)
+
+#call create_ladder function  
+create_ladder()
