19e39487e672677b70efe8b47fa7ba97bee0a09d
repl.it user <replituser@example.com>
2020-10-13 09:54:58 +0000

    ascii_char

--- basics/functions/ascii_code.py
+++ basics/functions/ascii_code.py
@@ -1,6 +1,8 @@
+#user input
 print("Program Started!")
 print("Please enter a standard character")
 char = input()
+#lookup ascii code of the character the user input and display answer
 if len(char) == 1:
   print(f"The ASCII code for {char} is", ord(char))
 else:
