4116b8dd7fa77e0a37703cbd2f1bbe1025353d8b
repl.it user <replituser@example.com>
2020-10-17 11:01:37 +0000

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,47 @@
-print("hello world")
+import basics.while_loop.simple.py as simple
+import basics.output.ascii_art as ascii_art
+import basics.output.escape_characters as escape_characters
+import basics.output.multiline_messages as multiline_messages
+import basics.output.simple_message as simple_message
+import basics.output.ascii_art as ascii_art
+import basics.nested_loop.nested.py as nested
+import basics.nested_loop.nesting.py as nesting
+import basics.modules.guess_the_number as guess_the_number
+
+import basics.input.
+import basics.input.
+import basics.input.
+import basics.input.
+import basics.input.
+
+import basics.functions.
+import basics.functions.
+import basics.functions.
+import basics.functions.
+import basics.functions.
+import basics.functions.
+import basics.functions.
+import basics.functions.
+import basics.functions.
+import basics.functions.
+
+import basics.for_loop
+import basics.for_loop
+import basics.for_loop
+import basics.for_loop
+import basics.for_loop
+import basics.for_loop
+
+import basics.decisions.
+import basics.decisions.
+import basics.decisions.
+
+import basics.decisions.nested_decisions.
+import basics.decisions.nested_decisions.
+
+import basics.simple_decision.
+import basics.simple_decision.
+import basics.simple_decision.
+import basics.simple_decision.
+import basics.simple_decision.
+import basics.simple_decision.
