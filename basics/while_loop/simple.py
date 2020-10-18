978860b576721dcf845e8c1e06800cf0080db196
repl.it user <replituser@example.com>
2020-10-18 15:45:29 +0000

    main.py

--- basics/while_loop/simple.py
+++ basics/while_loop/simple.py
@@ -1,18 +1,14 @@
-<<<<<<< HEAD
-#User to input number of cables for removal
-print("How many cables should I remove?")
-#stored in variable
-cables = int(input())
-print(f"Ok starting with {cables} Cables")
-#removed cables variable set
-remcables = 0
-#while command to increment removed cables whilst this remains lower than requested number of cables for removal
-while (remcables < cables):
-  print("Removed cable")
-=======
-print("How many cables should I remove?")
-cables = input()
-print(f"Ok starting with {cables} Cables")
-remcables = 0
-while (remcables < cables):
-  remcables = remcables + 1
+def run():
+  # Ask user for number of cables
+  print("How many cables should I remove?")
+  cables_to_remove = int(input())
+
+  # Declare a control variable
+  cables_removed = 0
+
+  # Remove cables
+  print()
+
+  while (cables_removed < cables_to_remove):
+    print("Removed cable.")
+    cables_removed = cables_removed + 1
--- main.py
+++ main.py
@@ -1,47 +1,51 @@
-import basics.while_loop.simple.py as simple
-import basics.output.ascii_art as ascii_art
-import basics.output.escape_characters as escape_characters
-import basics.output.multiline_messages as multiline_messages
-import basics.output.simple_message as simple_message
-import basics.output.ascii_art as ascii_art
-import basics.nested_loop.nested.py as nested
-import basics.nested_loop.nesting.py as nesting
-import basics.modules.guess_the_number as guess_the_number
+import basics.while_loop.simple as simple
+simple.run()
 
-import basics.input.
-import basics.input.
-import basics.input.
-import basics.input.
-import basics.input.
+#import basics.output.ascii_art as ascii_art
+#import basics.output.escape_characters as escape_characters
+#import basics.output.multiline_messages as multiline_messages
+#import basics.output.simple_message as simple_message
+#import basics.output.ascii_art as ascii_art
 
-import basics.functions.
-import basics.functions.
-import basics.functions.
-import basics.functions.
-import basics.functions.
-import basics.functions.
-import basics.functions.
-import basics.functions.
-import basics.functions.
-import basics.functions.
+#import basics.nested_loop.nested.py as nested
+#import basics.nested_loop.nesting.py as nesting
 
-import basics.for_loop
-import basics.for_loop
-import basics.for_loop
-import basics.for_loop
-import basics.for_loop
-import basics.for_loop
+#import basics.modules.guess_the_number as guess_the_number
 
-import basics.decisions.
-import basics.decisions.
-import basics.decisions.
+#import basics.input.ascii_robot as asciirobot
+#import basics.input.data_types as data_types
+#import basics.input.review as review
+#import basics.input.string_operators as string_operators
+#import basics.input.user_input as user_input
 
-import basics.decisions.nested_decisions.
-import basics.decisions.nested_decisions.
+#import basics.functions.ascii_characters as ascii_characters
+#import basics.functions.ascii_code as code
+#import basics.functions.function_calls as function_calls
+#import basics.functions.function_with_loop as function_with_loop
+#import basics.functions.function_with_nesting as function_with_nesting
+#import basics.functions.function_with_parameter as function_with_parameter
+#import basics.functions.function_with_parameters as function_with_parameters
+#import basics.functions.multiple_functions as multiple_functions
+#import basics.functions.return_values as return_values
+#import basics.functions.simple_function as simple_function
 
-import basics.simple_decision.
-import basics.simple_decision.
-import basics.simple_decision.
-import basics.simple_decision.
-import basics.simple_decision.
-import basics.simple_decision.
+#import basics.for_loop.characters as characters
+#import basics.for_loop.countdown as countdown
+#import basics.for_loop.membership_operators as membership_operators
+#import basics.for_loop.range as loop_range
+#import basics.for_loop.reverse as reverse
+#import basics.for_loop.simple as simple
+
+#import basics.decisions.and_operator as and_operator
+#import basics.decisions.or_operator as or_operator
+#import basics.decisions.review as review
+
+#import basics.decisions.nested_decisions.nestception as nestception
+#import basics.decisions.nested_decisions.nested as nested
+
+#import basics.simple_decision.comparison_operator as comparison_operator
+#import basics.simple_decision.counter as counter
+#import basics.simple_decision.if_elif_else as if_elif_else
+#import basics.simple_decision.if_else as if_else
+#import basics.simple_decision.simple_decision_if as simple_decision_if
+#import basics.simple_decision.modulo_operator as modulo_operator
