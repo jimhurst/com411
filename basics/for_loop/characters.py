d7e8f66cd83d713097da5c4abdcfed4ab152c33b
repl.it user <replituser@example.com>
2020-10-20 09:15:01 +0000

    simple_list

--- basics/for_loop/characters.py
+++ basics/for_loop/characters.py
@@ -1,9 +1,10 @@
-#Ask for user input and store in variable
-print("What strange markings do you see?")
-strange_input = input()
-print("Identifying....")
-print("")
-for position in range (0, len(strange_input),1):
-  print(f"Index {position}:", strange_input[position])
-print("")
-print("All Done!")
+def run():
+  #Ask for user input and store in variable
+  print("What strange markings do you see?")
+  strange_input = input()
+  print("Identifying....")
+  print("")
+  for position in range (0, len(strange_input),1):
+    print(f"Index {position}:", strange_input[position])
+  print("")
+  print("All Done!")
--- basics/functions/ascii_character.py
+++ basics/functions/ascii_character.py
@@ -1,16 +1,17 @@
-#user input
-print("Program Started!")
-print("Please enter an ASCII code")
-#store to variable as integer
-code = abs(int(input()))
-#set variables to use with range
-lowv = 32
-highv = 127
-#check that input is within range and execute ord command to lookup
-if code in range(lowv, highv):
-  letter = chr(code)
-  print("The character represented by the ASCII code {} is {}".format(code,letter))
-else:
-  print("The value entered is outside of the ASCII code range")
-#program end message
-print("Program Ended!")
+def run():
+  #user input
+  print("Program Started!")
+  print("Please enter an ASCII code")
+  #store to variable as integer
+  code = abs(int(input()))
+  #set variables to use with range
+  lowv = 32
+  highv = 127
+  #check that input is within range and execute ord command to lookup
+  if code in range(lowv, highv):
+    letter = chr(code)
+    print("The character represented by the ASCII code {} is {}".format(code,letter))
+  else:
+    print("The value entered is outside of the ASCII code range")
+  #program end message
+  print("Program Ended!")
