38cc0cfc74338adc96eee19dd3712faf56df929e
repl.it user <replituser@example.com>
2020-10-27 10:09:20 +0000

    simple_set

---
+++ data/sets/simple_set.py
@@ -0,0 +1,9 @@
+def observed():
+  observations = set()
+  observations = {"Flying Car", "Sky Scraper", "Laser", "Dome"}
+  return observations
+
+def run():
+  print(observed())
+
+run()
--- data/tuples/function_tuple.py
+++ data/tuples/function_tuple.py
@@ -9,3 +9,14 @@ def run():
   print("Minimum likelihood of falling: {}%".format(maxans))
 
 run()
+
+#def likelihood():
+#  likelihoods = (50, 38, 27, 99, 4)
+#  return min(likelihoods), max(likelihoods)
+#
+#def run():
+#  probabilities = likelihood()
+#  print(f"Minimum likelihood of falling: {probabilities[0]}%")
+#  print(f"Maximum likelihood of falling: {probabilities[1]}%")
+
+run()
