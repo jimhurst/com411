077300613e7dd7291acab7e6de37b7d5750bbc33
repl.it user <replituser@example.com>
2020-11-10 11:26:52 +0000

    path

--- visual/plots/path.py
+++ visual/plots/path.py
@@ -1,11 +1,25 @@
-#import pyplot module
-import matplotlib.pyplot as plt
-
 def coordinate():
   print("Please enter the value of x")
-  xvalue = int.input()
+  xvalue = int(input())
   print("Please enter the value of y")
-  yvalue = int.input()
+  yvalue = int(input())
   return (xvalue, yvalue)
 
-coordinate()
+def path():
+  print("Retrieving path...")
+  xvalues = []
+  yvalues = []
+  for count in range(4):
+    data = coordinate()
+    xvalues.append(data[0])
+    yvalues.append(data[1])
+  return [xvalues, yvalues]
+
+def run():
+  values = path()
+  #print(values)
+  import matplotlib.pyplot as plt
+  plt.plot(values[0], values[1], 'ro--')
+  plt.show()
+
+run()
